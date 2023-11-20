2b52ee31a35bafdedec6df74eb32fbdcbb54c72a
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2023-11-20 02:54:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const botaoMenu = document.querySelector(".cabecalho__menu")
-const menu = document.querySelector(".menu-lateral")
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
 
-botaoMenu.addEventListener("click", () => {
-    menu.classList.toggle("menu-lateral--ativos")
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
 })
